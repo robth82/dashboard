5ecc2ab632a0e45c519fc9ce6d6784bc4efaa04e
Rob ter Haar <r.terhaar@i-real.nl>
2016-06-29 23:03:56 +0200

    * typo in index.php

--- index.php
+++ index.php
@@ -5,11 +5,11 @@ use Robth82\Dashboard\Dashboard;
 include('bootstrap.php');
 
 //session_destroy();
-$dashboard = new Dashboard('3', new \Robth82\Dashboard\Store\SessionStore(), $dashboardCollection);
+$dashboard = new Dashboard('3', $store, $dashboardCollection);
 
 $template = $twig->loadTemplate('dashboard.twig');
 
-$template = $template->render(array('dashboard' => $dashboard, 'generatedJavascript' => \Robth82\Dashboard\Helpers\javascriptLoader::getScript()));
+$template = $template->render(array('dashboard' => $dashboard, 'generatedJavascript' => \Robth82\Dashboard\Helpers\JavascriptLoader::getScript()));
 
 echo $template;
 
